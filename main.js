b4ce7cb2c2182eb2c6eff0ee70dbbfe8d29a5059
DatNguyen139 <tt.dat.nguyen@tomosia.com>
2022-04-19 17:31:47 +0700

    check task completed

--- main.js
+++ main.js
@@ -1,14 +1,68 @@
-// export {}
 var newTask = document.querySelector(".newtask");
 var newList = document.querySelector(".middle");
+var footer = document.querySelector(".bottom");
+var selectAll = document.querySelector(".selectall");
 ;
-var todo;
-todo = JSON.parse((localStorage.getItem("listTodo") || '{}'));
+var todos;
+todos = JSON.parse((localStorage.getItem("listTodo") || '[]'));
+function countTaskActive() {
+    var countTaskActive = todos.length;
+    var countTaskCompleted = 0;
+    todos.forEach(function (data) {
+        if (data.complete === "completed") {
+            countTaskActive -= 1;
+            countTaskCompleted += 1;
+        }
+    });
+    document.querySelector("strong").innerHTML = "countTaskActive";
+    if (countTaskActive == 0 && countTaskCompleted == 0) {
+        footer.setAttribute("style", "display: none");
+        selectAll.setAttribute("style", "display: none");
+    }
+    else {
+        footer.removeAttribute("style", "display: none");
+        selectAll.removeAttribute("style", "display: none");
+    }
+}
+function editTask(event) {
+    var edit_task = event.parentElement.firstElementChild;
+    edit_task.removeAttribute("readonly", "readonly");
+    edit_task.addEventListener('mouseout', function () {
+        edit_task.setAttribute("readonly", "readonly");
+        if (edit_task.value.trim() === "") {
+            alert("please fill the edit");
+        }
+        else {
+            todos[event.id].task = edit_task.value;
+        }
+        localStorage.setItem("listTodo", JSON.stringify(todos));
+    });
+}
+function CheckComplete(selectask) {
+    console.log(selectask);
+    var check = selectask.parentElement.firstElementChild;
+    var index = parseInt(check.id);
+    if (selectask.checked) {
+        check.classList.add("checked");
+        todos[index].complete = "completed";
+    }
+    else {
+        check.classList.remove("checked");
+        todos[index].complete = "active";
+    }
+    localStorage.setItem("listTodo", JSON.stringify(todos));
+}
+function DeleteTask(index) {
+    var localitem = JSON.parse(localStorage.getItem("listTodo") || '{}');
+    todos.splice(index, 1);
+    localStorage.setItem("listTodo", JSON.stringify(todos));
+    ShowListTask();
+}
 function ShowListTask() {
     var html = "";
-    if (todo) {
-        todo.forEach(function (data, id) {
-            html += "\n          <div class=\"to-do\">\n            <input type=\"text\" class=\"text\" id = \"".concat(id, "\" value = \"").concat(data.task, "\" ondblclick = \"editlist(this)\" readonly>\n            <input type=\"checkbox\" class=\"checkbox\" id = \"").concat(id, "\" onclick = \"checkstatus(this)\" >\n            <button class=\"delete\" onclick = \"deletelist()\">Delete</button>\n          </div>");
+    if (todos) {
+        todos.forEach(function (data, id) {
+            html += "\n      <div class=\"to-do\">\n        <input type=\"text\" class=\"text\" id = \"".concat(id, "\" value = \"").concat(data.task, "\" ondblclick = \"editTask(this)\" readonly>\n        <input type=\"checkbox\" class=\"checkbox\" id = \"").concat(id, "\" onclick = \"CheckComplete(this)\" >\n        <button class=\"delete\" onclick = \"DeleteTask()\">Delete</button>\n      </div>");
         });
         newList.innerHTML = html;
     }
@@ -20,15 +74,15 @@ newTask.addEventListener('keyup', function (event) {
         alert("please fill in task input");
     }
     else if (event.key === "Enter" && inputValue !== "") {
-        if (!todo) {
-            todo = [];
+        if (!todos) {
+            todos = [];
         }
         var obj = {
             task: inputValue,
             complete: "active"
         };
-        todo.push(obj);
-        localStorage.setItem("listTodo", JSON.stringify(todo));
+        todos.push(obj);
+        localStorage.setItem("listTodo", JSON.stringify(todos));
         newTask.value = "";
         ShowListTask();
     }
